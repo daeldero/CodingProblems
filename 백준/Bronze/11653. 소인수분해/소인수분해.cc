66323f918475b8e4e870f98387ce2d61784b8774
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-08 14:07:59 +0900

    [Bronze I] Title: 소인수분해, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	int i = 2;
+	while (n != 1) {
+		while (n % i == 0) {
+			cout << i << '\n';
+			n /= i;
+		}
+		++i;
+	}
+
+	return 0;
+}
