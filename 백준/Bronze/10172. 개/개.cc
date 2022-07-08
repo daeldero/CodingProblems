186218ad52162cb4064c7d9a02d2d000e06f559d
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-08 13:04:42 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	cout << "|\\_/|" << '\n'
+		<< "|q p|   /}" << '\n'
+		<< "( 0 )\"\"\"\\" << '\n'
+		<< "|\"^\"`    |" << '\n'
+		<< "||_/=\\\\__|";
+
+	return 0;
+}
