83504b4175424f46a72f841576a2dd951bff996a
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-10 00:50:02 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	string s;
+	getline(cin, s, '\0');
+	cout << s;
+
+	return 0;
+}
