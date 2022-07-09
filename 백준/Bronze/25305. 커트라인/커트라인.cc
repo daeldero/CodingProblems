cc7cd4f8e44baaff2be8525bf24a29b302f448df
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-10 00:54:01 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int n, k;
+	cin >> n >> k;
+
+	vector<int> arr(n);
+	for (auto& elt : arr) cin >> elt;
+	sort(begin(arr), end(arr), greater<int>());
+
+	cout << arr[k - 1];
+
+	return 0;
+}
