9c159995067713ce2bbffe75d217163c2b06c0f8
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-10 00:47:25 +0900

    [Silver IV] Title: 큐, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	queue<int> arr;
+
+	for (int i = 0; i < n; ++i) {
+		string s;
+		cin >> s;
+
+		if (s == "push") {
+			int x;
+			cin >> x;
+			arr.push(x);
+		}
+		if (s == "pop") {
+			if (arr.empty()) cout << -1 << '\n';
+			else {
+				cout << arr.front() << '\n';
+				arr.pop();
+			}
+		}
+		if (s == "size") cout << arr.size() << '\n';
+		if (s == "empty") cout << arr.empty() << '\n';
+		if (s == "front") {
+			if (arr.empty()) cout << -1 << '\n';
+			else cout << arr.front() << '\n';
+		}
+		if (s == "back") {
+			if (arr.empty()) cout << -1 << '\n';
+			else cout << arr.back() << '\n';
+		}
+	}
+
+	return 0;
+}
