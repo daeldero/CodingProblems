91d4f50812fe2669d7b388cf06e0a3e5b00ae4bd
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-10 01:16:32 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	queue<int> card;
+	for (int i = 1; i <= n; ++i) card.push(i);
+
+	while (card.size() != 1) {
+		card.pop();
+		card.push(card.front());
+		card.pop();
+	}
+
+	cout << card.front();
+
+	return 0;
+}
