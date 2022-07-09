d128bb7a1bfc402731d520032e9ae0de1f34692b
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-10 01:18:42 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	for (int i = n; i > 0; --i) {
+		for (int j = 0; j < i; ++j) cout << '*';
+		cout << '\n';
+	}
+	
+	return 0;
+}
