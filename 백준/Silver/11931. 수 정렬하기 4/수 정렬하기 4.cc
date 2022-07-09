05b8070fdfbb814f0b7c43be53bf1a0154f80125
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-10 00:50:16 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 280 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	vector<int> arr(n);
+	for (auto& elt : arr) cin >> elt;
+	sort(begin(arr), end(arr));
+	
+	for (auto iter = rbegin(arr); iter != rend(arr); ++iter) cout << *iter << '\n';
+
+	return 0;
+}
