a8912e53c66db83ead0ec98a7c668a3ff205477c
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-10 01:40:14 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 308 ms, Memory: 3976 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	vector<int> arr(n);
+	for (auto& elt : arr) cin >> elt;
+	sort(begin(arr), end(arr));
+
+	int m;
+	cin >> m;
+	for (int i = 0; i < m; ++i) {
+		int x;
+		cin >> x;
+		cout << upper_bound(begin(arr), end(arr), x) - lower_bound(begin(arr), end(arr), x) << ' ';
+	}
+
+	return 0;
+}
