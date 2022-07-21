bb12e918006542a90a0c7b2d1746068a04878896
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-22 00:59:57 +0900

    Update 2016년.cpp

--- 프로그래머스/lv1/12901. 2016년/2016년.cpp
+++ 프로그래머스/lv1/12901. 2016년/2016년.cpp
@@ -7,10 +7,9 @@ vector<int> month_days{ 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 vector<string> days{ "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
 
 string solution(int a, int b) {
-    int dist = 5;
+    int dist = b + 4;
     for (int i = 1; i < a; ++i) {
         dist += month_days[i];
     }
-    dist += (b - 1);
     return days[dist % 7];
 }
