a22bf05687e351fa5612293c2996f687197b79ca
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-21 21:48:59 +0900

    Update 정수 내림차순으로 배치하기.cpp

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -4,9 +4,8 @@
 using namespace std;
 
 long long solution(long long n) {
-    long long answer = 0;
     string s = to_string(n);
     sort(begin(s), end(s), greater<char>());
-    answer = stoll(s);
+    long long answer = stoll(s);
     return answer;
 }
