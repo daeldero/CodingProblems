10a60e9eabccb97ec547e5ce22cde64fdd682769
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-21 21:48:23 +0900

    Update 정수 내림차순으로 배치하기.cpp

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -1,17 +1,12 @@
 #include <string>
 #include <vector>
-#include <string>
 #include <algorithm>
 using namespace std;
 
 long long solution(long long n) {
     long long answer = 0;
-    string s;
-    while (n != 0) {
-        s += static_cast<char>(n % 10 + '0');
-        n /= 10;
-    }
-    sort(begin(s), end(s), greater<long long>());
+    string s = to_string(n);
+    sort(begin(s), end(s), greater<char>());
     answer = stoll(s);
     return answer;
 }
