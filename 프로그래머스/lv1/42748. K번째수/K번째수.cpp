371d028f4db835aa2f377e22b5b768210b02d5ce
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-22 17:54:02 +0900

    Update K번째수.cpp

--- 프로그래머스/lv1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/lv1/42748. K번째수/K번째수.cpp
@@ -17,3 +17,18 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     
     return answer;
 }
+
+/*
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+	vector<int> answer;
+
+	for (const auto& command : commands) {
+		vector<int> temp(command[1] - command[0] + 1);
+		copy(begin(array) + command[0] - 1, begin(array) + command[1], begin(temp));
+		sort(begin(temp), end(temp));
+		answer.push_back(temp[command[2] - 1]);
+	}
+
+	return answer;
+}
+*/
