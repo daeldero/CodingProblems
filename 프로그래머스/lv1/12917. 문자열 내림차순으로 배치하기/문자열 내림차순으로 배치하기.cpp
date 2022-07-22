f0809dd847fa577211b5702ffb179700b025b16a
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-22 16:42:02 +0900

    Update 문자열 내림차순으로 배치하기.cpp

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cpp
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cpp
@@ -6,5 +6,6 @@ using namespace std;
 string solution(string s) {
     string answer = s;
     sort(begin(answer), end(answer), greater<char>());
+    // sort(rbegin(answer), rend(answer));
     return answer;
 }
