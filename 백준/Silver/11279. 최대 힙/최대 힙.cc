41e6adc114a0f6f61b650676bdf0e50ce5f226ea
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-22 11:24:46 +0900

    [Silver II] Title: 최대 힙, Time: 40 ms, Memory: 6776 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	multiset<int, greater<int>> arr;
+	for (int i = 0; i < n; ++i) {
+		int x;
+		cin >> x;
+		if (x == 0) {
+			if (arr.empty()) cout << "0\n";
+			else {
+				cout << *begin(arr) << '\n';
+				arr.erase(begin(arr));
+			}
+		}
+		else arr.insert(x);
+	}
+
+	return 0;
+}
