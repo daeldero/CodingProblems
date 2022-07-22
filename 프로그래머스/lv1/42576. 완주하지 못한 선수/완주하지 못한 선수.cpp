46409a9bed9a87104f7d1fd1dd64e82190c5b755
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-22 17:05:30 +0900

    Update 완주하지 못한 선수.cpp

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -12,3 +12,24 @@ string solution(vector<string> participant, vector<string> completion) {
     }
     return *begin(arr);
 }
+
+/*
+string solution(vector<string> participant, vector<string> completion) {
+	char answer[21];
+	for (int j = 0; j < 21; ++j) answer[j] = 0;
+
+	int n = participant.size();
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < participant[i].size(); ++i) {
+			answer[j] ^= participant[i][j];
+		}
+	}
+
+	for (int i = 0; i < n - 1; ++i) {
+		for (int j = 0; j < completion[i].size(); ++j) {
+			answer[j] ^= completion[i][j];
+		}
+	}
+	return string(answer);
+}
+*/
