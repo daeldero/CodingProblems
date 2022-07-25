9e6b2b45572138b79bc53b91a0d79d056ec46b2e
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-25 22:22:48 +0900

    [Bronze IV] Title: 평균 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int sum = 0;
+	for (int i = 0; i < 5; ++i) {
+		int n;
+		cin >> n;
+		if (n < 40) n = 40;
+		sum += n;
+	}
+	cout << sum / 5;
+
+	return 0;
+}
