28fe484bc13364e21e23d87d7737211afd0dc7a5
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-25 16:08:14 +0900

    [Silver III] Title: 피자 (Large), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14607. 피자 （Large）/피자 （Large）.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	long long n;
+	cin >> n;
+	cout << n * (n - 1) / 2;
+
+	return 0;
+}
